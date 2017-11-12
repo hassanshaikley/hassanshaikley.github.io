0b9e2c45ad4fac4d206cbf6cabaf4679d97601ff
Hassan Shaikley <hassan.shaikley@gmail.com>
2017-11-12 16:45:46 -0700

    Updates

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: `Hassan Shaikley Portfolio`,
-		description: `A talented Software Engineer currently Residing in Calgary AB..`
-  },
-  plugins: [`gatsby-plugin-react-helmet`],
-	pathPrefix: `/hassanshaikley.github.io`
-}
