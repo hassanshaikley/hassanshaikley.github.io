13bd1f259aacd3c7569099e8e11afe3268de053e
Hassan Shaikley <hassan.shaikley@gmail.com>
2018-01-17 20:31:24 -0700

    ittle fix

--- src/components/right.js
+++ src/components/right.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export const Right = ({children}) => (
-  <div style={{ width: '60%'}}>
+  <div style={{ width: '60%', minWidth: '200px'}}>
     {children}
   </div>
 )
