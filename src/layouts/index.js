a6264495f122d9732aa7eb0c723aaaabbe98de3a
Hassan Shaikley <hassan.shaikley@gmail.com>
2021-08-11 02:51:40 -0400

    progress

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -22,18 +22,21 @@ const Header = () => (
     }} id="header">
     <div style={{ }}>
       <div style={{  }}>
-        <h1 style={{ margin: 0, padding: "30% 2rem 0", textAlign: 'center'}}>
+        <h1 id="header-h1" style={{ margin: 0, textAlign: 'center'}}>
         Hassan Shaikley
         </h1>
-        <div style={{margin: 'auto'}}>
-        <a id="left-small">
-        Home
+        <div style={{margin: 'auto', width: 'max-content', marginTop: '1em'}}>
+        <a id="left-small" href="#about_me">
+        About Me
         </a>
-        <a id="left-small">
-        About
+        <a id="left-small" href="#blog">
+        Blog
         </a>
-        <a id="left-small">
+        <a id="left-small" href="#projects">
         Projects
+        </a>
+        <a id="left-small" href="#talks">
+        Talks
         </a>
         </div>
       </div>
@@ -99,39 +102,65 @@ const Footer = () => (
   </div>
 )
 
-const about_me_body = () =>         <p>
-Hi! I'm Hassan Shaikley. I am currently a Software Engineer at
-Community where I write & review code written in Elixir.
+// const about_me_body = () =>         <p>
+// Hi! I'm Hassan Shaikley. I am currently a Software Engineer at
+// Community where I write & review code written in Elixir.
+// <br />
+// <br />
+// I received my Bachelors of Science in Computer Science from the
+// University of California San Diego in 2016. In my third year I left to
+// focus on a startup that went viral and earned a spot at a startup
+// accelerator. Afterwards I went back to finish my degree.
+// <br />
+// <br />
+// I am really interested in functional programming, React & creating
+// beautiful and organized frontends, game servers, Elixir/Erlang/BEAM,
+// and distributed computation as well as embedded programming with
+// Nerves. I am also interested in Rust but have yet to start using it.
+// Lately I have been too busy tinkering with the BEAM, three.js and
+// nerves. I feel very fond towards the BEAM due to it being an ingenius
+// implementation of the actor model.
+// <br />
+// <br />I spend my free time hanging out with my beautiful wife, walking
+// my chihuahua, tinkering with hardware and software, playing the
+// guitar, gardening, making games, playing Mercy on Overwatch and
+// playing Animal Crossing.
+// </p>
+
+
+const about_me_body = () => <p>
+My name is Hassan. I live in Atlanta, Georgia. In the past I've lived in Calgary, Somerville, Mountain View and Los Angeles.
 <br />
 <br />
-I received my Bachelors of Science in Computer Science from the
-University of California San Diego in 2016. In my third year I left to
-focus on a startup that went viral and earned a spot at a startup
-accelerator. Afterwards I went back to finish my degree.
+I spend my time in my garden, playing video games, hanging out with my gorgeous wife and with my pets.
 <br />
 <br />
-I am really interested in functional programming, React & creating
-beautiful and organized frontends, game servers, Elixir/Erlang/BEAM,
-and distributed computation as well as embedded programming with
-Nerves. I am also interested in Rust but have yet to start using it.
-Lately I have been too busy tinkering with the BEAM, three.js and
-nerves. I feel very fond towards the BEAM due to it being an ingenius
-implementation of the actor model.
+I've co-founded a startup. Worked directly with many CEOs. In one of my roles I was a director at a software company with clients across North America.
+I love to build prototypes and test the limits of new technology. Feel free to ask; I enjoy talking about all of my experiences.
 <br />
-<br />I spend my free time hanging out with my beautiful wife, walking
-my chihuahua, tinkering with hardware and software, playing the
-guitar, gardening, making games, playing Mercy on Overwatch and
-playing Animal Crossing.
+<br />
+I am a huge fan of Elixir and React. There's a whole laundry list of technologoies I'm interested in which include
+Zig, Godot Engine, three.js, Gatsby, React Three Fiber and many others.
+My experience is fairly broad; I've taken ideas into production using many different technologies. 
+<br />
+<br />
+I love to build! I turned a nightmare I had as a child into a game which can be downloaded <a href="https://github.com/hassanshaikley/scary-spoders/releases">here</a>.
+<br />
+<br />
+I've contributed to many open source projects including <a href="https://github.com/elixir-lang/elixir/">Elixir</a>, <a href="https://github.com/pubpub/pubpub">PubPub</a>, <a href="https://github.com/hassanshaikley/svg-pan-zoom">SVG Pan Zoom</a> and some others.
 </p>
 
 const projects_body = () =>         <p>List of Prijects</p>
 
-const tech_talks_body = () =>         <p>List of tech Talks</p>
+const talks_body = () =>         <p>I'm going to be doing my first tech talk very soon at NervesConfon Building a Drum Machine with a Raspberry Pi and Nerves.</p>
 
+const blog_body = () => <p>
+  <a href="https://hassanshaikley.medium.com/">Link to my blog.</a>
+</p>
 
 
 const section =  (title, body) => <div>
-<h2>{title}</h2>
+<h2 id={title.toLowerCase().split(' ').join('_')}>{title}</h2>
 <p>
   {body()}
 </p>
@@ -154,9 +183,9 @@ const TemplateWrapper = ({ children }) => (
 
     <div style={{display: 'inline-block'}}>
       {section("About Me", about_me_body)}
-      {section("projects", projects_body)}
-      {section("tech_talks", tech_talks_body)}
-
+      {section("Blog", blog_body)}
+      {section("Projects", projects_body)}
+      {section("Talks", talks_body)}
 
 
     </div>
